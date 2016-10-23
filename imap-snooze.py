5bac5b63d338c66b13f2004f20693ea39b19ef77
Tobias Grosser <tobias@grosser.es>
2016-10-23 18:44:00 +0200

    Closer to reality

--- imap-snooze.py
+++ imap-snooze.py
@@ -40,16 +40,17 @@ class IMAPSnoozeDaemon:
     def findSnoozeBoxes(self):
         status , mailboxes = self.imap.lsub()
         assert(status == "OK")
-        snoozeboxes = filter(lambda x : str(x).find("snooze") != -1, mailboxes)
+        snoozeboxes = filter(lambda x : str(x).find("pop") != -1, mailboxes)
         self.boxes = list(map(SnoozeBox, snoozeboxes))
         print(self.boxes)
 
     def loop(self):
         while True:
+            print("Checking mailbox again")
             for box in self.boxes:
                 self.process(box)
             import time
-            time.sleep(5)
+            time.sleep(60)
 
     def process(self, box):
         self.markNew(box)
@@ -72,7 +73,7 @@ class IMAPSnoozeDaemon:
             if body.find("X-SNOOZE:") == -1:
                 import time
                 t = int(time.time())
-                t += int(box.time) * 10
+                t += int(box.time) * 3600
                 body = "X-SNOOZE: " + str(t) + "\n" + body
                 toDelete.append(mail)
                 self.imap.append(box.name, None, None, body.encode("utf-8"))
@@ -94,11 +95,31 @@ class IMAPSnoozeDaemon:
             status, body = self.imap.uid('fetch', mail, '(RFC822)')
             assert(status == "OK")
             body = body[0][1].decode("utf-8")
+            import email.feedparser as parser
+            p = parser.FeedParser()
+            p.feed(body)
+            email = p.close()
+
             if body.find("X-SNOOZE:") != -1:
                 import re
                 moveTime = int(re.findall(r'\d+', body) [0])
                 import time
-                if moveTime > time.time():
+                currentTime = time.time()
+                if moveTime > currentTime:
+                    remainingTime = moveTime - currentTime
+                    remainingSeconds = remainingTime % 60
+                    remainingMinutes = remainingTime % (60 * 60)
+                    remainingHours = remainingTime % (60 * 60 * 60)
+                    remainingDays = remainingTime
+
+                    remainingSeconds = remainingSeconds
+                    remainingMinutes = remainingMinutes / 60
+                    remainingHours = remainingHours / (60 * 60)
+                    remainingDays = remainingDays / (60 * 60 * 24)
+
+                    print("# Igoring\n\"%s\" from %s" %
+                          (email["Subject"], email["From"]))
+                    print("Time left: %d d, %d h, %d m, %d s" % (remainingDays, remainingHours, remainingMinutes, remainingSeconds))
                     continue
                 body = body[body.find('\n')+1:body.rfind('\n')]
                 self.imap.append("INBOX", None, None, body.encode("utf-8"))
